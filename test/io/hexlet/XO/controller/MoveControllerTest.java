a1ebc24efd17eabc38e85cfcf420fafa14c0376c
Basil <basil135@mail.ru>
2016-09-08 17:25:00 +0300

    MoveControllerTest - create

---
+++ test/io/hexlet/XO/controller/MoveControllerTest.java
@@ -0,0 +1,57 @@
+package io.hexlet.XO.controller;
+
+import io.hexlet.XO.model.Field;
+import io.hexlet.XO.model.Figure;
+import io.hexlet.XO.model.Point;
+import io.hexlet.XO.model.exception.AlreadyPointExistException;
+import io.hexlet.XO.model.exception.InvalidPointException;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MoveControllerTest {
+    @Test
+    public void applyFigure() throws Exception {
+
+        Field field = new Field();
+
+        MoveController moveController = new MoveController();
+
+        Figure testValue = Figure.X;
+
+        for (int i = 0; i < field.getsize(); i++) {
+
+            checkRow(field, moveController, i, testValue);
+
+        }
+
+        Field field1 = new Field();
+
+        testValue = Figure.O;
+
+        for (int i = 0; i < field1.getsize(); i++) {
+
+            checkRow(field1, moveController, i, testValue);
+
+        }
+
+    }
+
+    private void checkRow(Field field, MoveController moveController, int i, Figure testValue) throws InvalidPointException, AlreadyPointExistException {
+
+        for (int j = 0; j < field.getsize(); j++) {
+
+            Point point = new Point(i, j);
+
+            Figure expectedValue = testValue;
+
+            moveController.applyFigure(field, point, testValue);
+
+            Figure actualValue = field.getfigure(point);
+
+            assertEquals(expectedValue, actualValue);
+        }
+
+    }
+
+}
