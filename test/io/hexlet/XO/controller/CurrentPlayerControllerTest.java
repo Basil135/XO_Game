85165c61f27208e136d3fa3b82820c584393a1ad
Basil <basil135@mail.ru>
2016-09-09 16:18:11 +0300

    CurrentPlayerControllerTest - fix bugs

--- test/io/hexlet/XO/controller/CurrentPlayerControllerTest.java
+++ test/io/hexlet/XO/controller/CurrentPlayerControllerTest.java
@@ -35,7 +35,7 @@ public class CurrentPlayerControllerTest {
     @Test
     public void checkMoveWhenOneFigureIsSet() throws Exception {
 
-        field.setfigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(0, 0), Figure.X);
 
         testValue = Figure.O;
 
@@ -50,8 +50,8 @@ public class CurrentPlayerControllerTest {
     @Test
     public void checkMoveWhenTwoFigureIsSet() throws Exception {
 
-        field.setfigure(new Point(0, 0), Figure.X);
-        field.setfigure(new Point(1, 1), Figure.O);
+        field.setFigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.O);
 
         testValue = Figure.X;
 
@@ -60,6 +60,26 @@ public class CurrentPlayerControllerTest {
         actualValue = currentPlayerController.checkMove(field);
 
         assertEquals(expectedValue, actualValue);
+
+    }
+
+    @Test
+    public void checkMoveWhenFieldIsFull() throws Exception {
+
+        field.setFigure(new Point(1, 0), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(1, 2), Figure.O);
+        field.setFigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(0, 1), Figure.O);
+        field.setFigure(new Point(0, 2), Figure.O);
+        field.setFigure(new Point(2, 0), Figure.X);
+        field.setFigure(new Point(2, 1), Figure.O);
+        field.setFigure(new Point(2, 2), Figure.X);
+
+
+        actualValue = currentPlayerController.checkMove(field);
+
+        assertNull(actualValue);
 
     }
 
