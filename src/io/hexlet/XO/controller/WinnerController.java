2bbf1fdb3eb644c231f3892aba551679020ec784
Basil <basil135@mail.ru>
2016-09-09 13:12:44 +0300

    WinnerController - create

---
+++ src/io/hexlet/XO/controller/WinnerController.java
@@ -0,0 +1,64 @@
+package io.hexlet.XO.controller;
+
+import io.hexlet.XO.model.*;
+
+public class WinnerController {
+
+    public Figure checkWinner(final Field field) {
+
+        for (int i = 0; i < field.getsize(); i++) {
+            if (check(field, new Point(i, 0), new INextPoint() {
+                @Override
+                public Point nextPoint(Point point) {
+                    return new Point(point.getX(), point.getY() + 1);
+                }
+            })) {return field.getfigure(new Point(i, 0));}
+        }
+
+        for (int i = 0; i < field.getsize(); i++) {
+            if (check(field, new Point(0, i), new INextPoint() {
+                @Override
+                public Point nextPoint(Point point) {
+                    return new Point(point.getX() + 1, point.getY());
+                }
+            })) {return field.getfigure(new Point(0, i));}
+        }
+
+        if (check(field, new Point(0, 0), new INextPoint() {
+            @Override
+            public Point nextPoint(Point point) {
+                return new Point(point.getX() + 1, point.getY() + 1);
+            }
+        })) {return field.getfigure(new Point(0, 0));}
+
+        if (check(field, new Point(0, 2), new INextPoint() {
+            @Override
+            public Point nextPoint(Point point) {
+                return new Point(point.getX() + 1, point.getY() - 1);
+            }
+        })) {return field.getfigure(new Point(0, 2));}
+
+        return null;
+
+    }
+
+    private boolean check(Field field, Point startPoint, INextPoint nextPoint) {
+
+        Point p1 = startPoint;
+        Point p2 = nextPoint.nextPoint(p1);
+        Point p3 = nextPoint.nextPoint(p2);
+
+        if (field.getfigure(p1) == null) {return false;}
+        if (field.getfigure(p1) == field.getfigure(p2) && field.getfigure(p2) == field.getfigure(p3)) {return true;}
+
+        return false;
+
+    }
+
+    private interface INextPoint {
+
+        Point nextPoint(final Point point);
+
+    }
+
+}
